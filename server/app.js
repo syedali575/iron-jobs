08b5db7f4da4084904174db98153eaaf5eee7b6a
Syed Ali <sali3@cox.net>
2016-12-15 14:10:22 -0500

    Updated bash profile, created database, and working on creating server

--- server/app.js
+++ server/app.js
@@ -1,2 +1,27 @@
 
-console.log('Hello World');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var server = express();
+
+server.set('port', process.env.PORT || 3000);
+
+server.use(bodyParser.json());
+
+server.use(function logger(req, res, next){
+  console.log('REQUEST sent to', req.path);
+
+next();
+
+});
+
+
+
+server.listen(server.get('port'), function serverStarted(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Server started at http://localhost:' + server.get('port'));
+    }
+});
